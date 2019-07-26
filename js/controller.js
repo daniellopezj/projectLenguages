c5ace2929666a1282bbdc6510209482f9f32dfa4
daniel hizo un comentario <daniel.lopez01@uptc.edu.co>
2019-07-25 20:10:38 -0500

    v.0.1
    crear expresiones regulares

--- js/controller.js
+++ js/controller.js
@@ -1,40 +1,48 @@
 angular.module('AnalizadorLexico', [])
-  .controller('Controller', function ($scope, $http) {
-    var app = this;
-    app.textString = "";
-    app.listResult;
+    .controller('Controller', function($scope, $http) {
+        var app = this;
+        app.textString = "";
+        app.listResult;
+        app.verifySintaxis = function() {
+            app.listResult = [];
+            console.log(split(app.textString));
+            var listSplit = split(app.textString);
+            for (let i = 0; i < listSplit.length; i++) {
+                // console.log(variable.test(app.textString)); // esta linea retorna boolean si cumple la ER
+                if (For.test(listSplit[i]) == true || While.test(listSplit[i]) == true) {
+                    app.listResult.push("ciclo creado");
+                } else if (string.test(listSplit[i]) == true || number.test(listSplit[i]) == true || character.test(listSplit[i]) == true) {
+                    app.listResult.push("variable creada");
+                } else {
+                    app.listResult.push("error en la linea " + (i + 1));
+                }
+            }
 
 
-
-    app.verifySintaxis = function () {
-      app.listResult = [];
-      console.log(split(app.textString));
-      var listSplit = split(app.textString);
-      for (let i = 0; i < listSplit.length; i++) {
-        if (WordRerserve[listSplit[i]] !== undefined) {
-
-          app.listResult.push(WordRerserve[listSplit[i]] + " : Es una palabra reserveda");
-
-        } else if (Operator[listSplit[i]] !== undefined) {
-
-          app.listResult.push(listSplit[i] + "  " + Operator[listSplit[i]]);
-
-        } else {
-          app.listResult.push(listSplit[i] + " : Identificador");
+            /*
+                        for (let i = 0; i < listSplit.length; i++) {
+                            if (WordRerserve[listSplit[i]] !== undefined) {
+                                app.listResult.push(WordRerserve[listSplit[i]] + " : Es una palabra reserveda");
+                            } else if (Operator[listSplit[i]] !== undefined) {
+                                app.listResult.push(listSplit[i] + "  " + Operator[listSplit[i]]);
+                            } else {
+                                app.listResult.push(listSplit[i] + " : Identificador");
+                            }
+                        }*/
         }
 
-      }
-    }
+        function split(words) {
+            // console.log(words);
+            //   var text = words.replace(/[\s]+/g, " ").trim();
+            //  console.log(text);
 
-    function split(words) {
-      var text = words.replace(/[\s]+/g, " ").trim();
-      var wordSplit = text.split(" ");
-      return wordSplit;
-    }
+            var wordSplit = words.split('\n');
+            return wordSplit;
+        }
 
+        function cutword(listSplit) {
+            var array
+            for (let i = 0; i < listSplit.length; i++) {}
 
-
-
-
-  });
-
+        }
+    });
